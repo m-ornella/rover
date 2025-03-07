264a163725b72c1e38187d87c5a77d8ce6a3de84
LouisBrousse <brousselouis@hotmail.com>
2025-03-07 15:40:00 +0100

    maj main2

--- src/mission_control/main.py
+++
@@ -1,7 +0,0 @@
-from src.network.socket_network import SocketNetwork
-from src.mission_control.mission_control import MissionControl
-
-if __name__ == "__main__":
-    network = SocketNetwork(is_server=False)  # Mission Control agit comme un client
-    mission_control = MissionControl(communication=network)
-    mission_control.start()
